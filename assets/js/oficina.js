a1f6a70d6acc26fa00447edc6723ef5d34ca9c5c
WESLLEY HOLANDA SANTOS <weslley.hsantos2@senacsp.edu.br>
2024-10-17 07:58:24 -0300

    Primeira Versão

---
+++ assets/js/oficina.js
@@ -0,0 +1,19 @@
+// BANNER
+$('.slide-banner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 2000,
+});
+
+// Marcas
+
+$('.marcas-confiaveis .site div').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    dots: false,
+    autoplaySpeed: 2000,
+});
