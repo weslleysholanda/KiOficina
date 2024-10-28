f15739789425a965c7c55c47372b7f5914b9196d
WESLLEY HOLANDA SANTOS <weslley.hsantos2@senacsp.edu.br>
2024-10-28 11:55:10 -0300

    versão 1.1

--- core/Core.php
+++ core/Core.php
@@ -6,29 +6,31 @@ class Core{
     //Método inicializar o processo de ROTAS
     public function executar(){
         $url = '/';
-        // var_dump($url); 
+       
 
         //Verificar se a variavel URL esta definida na $_GET
         if(isset($_GET['url'])){
             $url .= $_GET['url'];
            
         }
+
+        // var_dump($url); 
         
         // Definindo um array para armazenar os parametros da URL
-        $parametro = array();
+        // $parametro = array();
 
-        //Verifica se a URL não está vazia e não é apenas uma /
+        // //Verifica se a URL não está vazia e não é apenas uma /
         if(!empty($url) && $url != '/'){
             // servicos/especialidade/nomeServico
             // controller/ação/parametro
             $url = explode('/', $url);
+            var_dump($url);
             
             // servicos[0]
             // especialidade[1]
             // nomeServico[2]
             
             array_shift($url);
-
             // var_dump($url);
 
             //Obter o controller atual
@@ -36,12 +38,18 @@ class Core{
             // . foi utilizado como Contatenação com o 'Controller' ao nome para seguir o padrão
             $controladorAtual = ucfirst($url[0]) . 'Controller'; 
 
+            array_shift($url);
+            // var_dump($url);
+
             // isset — Determina se uma variável está declarada e é diferente de null
             // empty - Determina se uma variável está vazia
             if(isset($url[0]) && !empty($url[0])){
                 $acaoAtual = $url[0];
+                // var_dump("Nome da ação atual: ".$acaoAtual);
+                array_shift($url);
             }else{
-                $acaoAtual = 'index';            
+                $acaoAtual = 'index';  
+                // var_dump($acaoAtual);     
             }
 
             // count— Conta todos os elementos em uma matriz ou em um objeto
@@ -59,15 +67,15 @@ class Core{
             if(!file_exists('../app/controllers/' . $controladorAtual . '.php') ||!method_exists($controladorAtual, $acaoAtual)){
 
                 //Se não existir defina o controller como ErroController
-                $controladorAtual = 'ErroController';
+                
                 $acaoAtual = 'index';
             }
         }
 
-        // Instancia do controller atual
-        $controller = new $controladorAtual();
+        // // Instancia do controller atual
+        // $controller = new $controladorAtual();
 
-        // call_user_func_array - Chama um retorno de chamada com uma matriz de parâmetros
-        call_user_func_array(array($controller, $acaoAtual), $parametro);
+        // // call_user_func_array - Chama um retorno de chamada com uma matriz de parâmetros
+        // call_user_func_array(array($controller, $acaoAtual), $parametro);
     }
 }
